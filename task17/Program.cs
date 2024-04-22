1a6070b9c5e5d32bc55717cff91a0ac63bef026d
Ramazan <bd5lkedwf@code.edu.az>
2024-04-23 03:04:04 +0400

    Task17

---
+++ task17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace task17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Name = "BMW",
+                Model = "F10",
+                Color = "Black",
+                Price = 28000
+            };
+            string carSer = JsonSerializer.Serialize(car);
+            Console.WriteLine(carSer);
+
+            string json = "{\"Name\":\"BMW\",\"Model\":\"F10\",\"Color\":\"Black\",\"Price\":28000}";
+            Car car1 = JsonSerializer.Deserialize<Car>(json);
+            Console.WriteLine($"Name: {car.Name}");
+            Console.WriteLine($"Model: {car.Model}");
+            Console.WriteLine($"Color: {car.Color}");
+            Console.WriteLine($"Price: {car.Price}");
+
+        }
+    }
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ task17_XML/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Xml.Serialization;
+
+namespace task17_XML
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student()
+            {
+                Name = "Ramazan",
+                Surname = "Asgerli",
+                Age = 20
+            };
+            Student student1 = new Student()
+            {
+                Name = "Rehman",
+                Surname = "Murselov",
+                Age = 20
+            };
+            Student student2 = new Student()
+            {
+                Name = "Elnur",
+                Surname = "Ferzaliyev",
+                Age = 20
+            };
+            string xmlString;
+            XmlSerializer serializeStudent = new XmlSerializer(typeof(Student));
+            using (StringWriter writer = new StringWriter())
+            {
+                serializeStudent.Serialize(writer, student);
+                serializeStudent.Serialize(writer, student1);
+                serializeStudent.Serialize(writer, student2);
+                xmlString = writer.ToString();
+                Console.WriteLine(xmlString);
+            }
+            //using (StringReader reader = new StringReader(xmlString))
+            //{
+            //    Student deserialize = (Student)serializeStudent.Deserialize(reader);
+            //    Console.WriteLine($"Student 1: {deserialize.Name} {deserialize.Surname} {deserialize.Age}");
+
+            //    deserialize = (Student)serializeStudent.Deserialize(reader);
+            //    Console.WriteLine($"Student 2: {deserialize.Name} {deserialize.Surname} {deserialize.Age}");
+
+            //    deserialize = (Student)serializeStudent.Deserialize(reader);
+            //    Console.WriteLine($"Student 3: {deserialize.Name} {deserialize.Surname} {deserialize.Age}");
+            //}
+        }
+
+
+    }
+    }
+    public class Student 
+    {
+        public static int _id;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set;}
+
+        public Student()
+        {
+            Id = ++_id;
+        }
+    }
+
+
